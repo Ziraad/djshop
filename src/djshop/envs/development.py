b74a431d9e6c31db07af599066d7c0d1dd61f016
ziraad <parand8631@gmail.com>
2023-09-03 14:02:38 +0330

    init commit

---
+++ src/djshop/envs/development.py
@@ -0,0 +1,19 @@
+from .common import *
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+                     'daphne',
+                     'drf_spectacular',
+                 ] + INSTALLED_APPS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djshop',
+        'USER': 'djshop',
+        'PASSWORD': '123@456',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}

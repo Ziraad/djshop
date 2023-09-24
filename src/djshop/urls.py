e54116bb498bdec845134c691d4f06368906b0fc
ziraad <parand8631@gmail.com>
2023-09-24 23:58:11 +0330

    create DuplicateImageException class for manage Exeption and check duplicate image updating and set MEDIA_URL in settings

--- src/djshop/urls.py
+++ src/djshop/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.conf import settings
 
 admin_url = [
     path('api/admin/catalog/', include(('djshop.apps.catalog.urls.admin', 'djshop.apps.catalog'), namespace='catalog'
@@ -39,6 +41,9 @@ doc_patterns = [
 urlpatterns = [
                   path("admin/", admin.site.urls),
               ] + front_url + admin_url + doc_patterns
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_title = 'DjShop'
 admin.site.index_title = 'DjShop'
